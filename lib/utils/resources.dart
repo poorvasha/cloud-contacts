1466346453d217918c7e068baf9aadbcf3f08d64
poorvasha <poorvashadmp@gmail.com>
2023-02-14 05:10:32 +0530

    add global resources required for the App

--- lib/utils/resources.dart
+++ lib/utils/resources.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class AppResources {}
+import '../models/input_field_data.dart';
+
+class AppResources {
+  static var screenMargin = const EdgeInsets.all(30);
+}
 
 // #region Colors
 class AppColors {
@@ -17,25 +22,25 @@ class AppColors {
 // #region TextStyles
 class AppTextStyles {
   static TextStyle semiBoldheaderStyle = const TextStyle(
-      fontFamily: 'Raleway', fontSize: 21, fontWeight: FontWeight.w600);
+      fontFamily: 'Raleway', fontSize: 21, fontWeight: FontWeight.w700);
 
   static TextStyle boldSubHeadingStyle = const TextStyle(
-      fontFamily: 'Quicksand', fontSize: 17, fontWeight: FontWeight.w700);
+      fontFamily: 'Quicksand', fontSize: 17, fontWeight: FontWeight.w800);
 
   static TextStyle semiBoldSubHeadingStyle = const TextStyle(
-      fontFamily: 'Quicksand', fontSize: 17, fontWeight: FontWeight.w600);
-
-  static TextStyle mediumSubHeadingStyle = const TextStyle(
       fontFamily: 'Quicksand', fontSize: 17, fontWeight: FontWeight.w700);
 
-  TextStyle boldContentStyle = const TextStyle(
+  static TextStyle mediumSubHeadingStyle = const TextStyle(
+      fontFamily: 'Quicksand', fontSize: 17, fontWeight: FontWeight.w600);
+
+  static TextStyle boldContentStyle = const TextStyle(
+      fontFamily: 'Quicksand', fontSize: 13, fontWeight: FontWeight.w800);
+
+  static TextStyle semiBoldContentStyle = const TextStyle(
       fontFamily: 'Quicksand', fontSize: 13, fontWeight: FontWeight.w700);
 
-  TextStyle semiBoldContentStyle = const TextStyle(
+  static TextStyle mediumContentStyle = const TextStyle(
       fontFamily: 'Quicksand', fontSize: 13, fontWeight: FontWeight.w600);
-
-  TextStyle mediumContentStyle = const TextStyle(
-      fontFamily: 'Quicksand', fontSize: 13, fontWeight: FontWeight.w700);
 }
 // #endregion
 
@@ -45,15 +50,36 @@ class AppShaddows {
     BoxShadow(
         color: AppColors.purple.withOpacity(0.15),
         spreadRadius: 0,
-        offset: const Offset(7, 7),
-        blurRadius: 12,
+        offset: const Offset(4, 4),
+        blurRadius: 8,
         blurStyle: BlurStyle.normal),
     BoxShadow(
         color: AppColors.white.withOpacity(0.60),
-        spreadRadius: 0,
-        offset: const Offset(-7, -7),
+        spreadRadius: 8,
+        offset: const Offset(-4, -4),
         blurRadius: 12,
         blurStyle: BlurStyle.normal)
   ];
 }
 // #endregion
+
+// #region Input Datas
+class AppInputDatas {
+  static List<InputFieldData> loginInputData = [
+    InputFieldData(null, Icons.person_rounded,
+        hintText: 'username',
+        errMessage: 'please enter valid username',
+        isValid: true,
+        myController: TextEditingController(),
+        keyboardType: TextInputType.emailAddress,
+        textInputType: FilteringTextInputFormatter.singleLineFormatter),
+    InputFieldData(null, Icons.lock_rounded,
+        hintText: 'password',
+        errMessage: 'please enter valid password',
+        isValid: true,
+        myController: TextEditingController(),
+        keyboardType: TextInputType.text,
+        textInputType: FilteringTextInputFormatter.singleLineFormatter)
+  ];
+}
+// #region
