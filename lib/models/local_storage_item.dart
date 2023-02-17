b2162e2f5679d5eaae1318ed1b449e09a7d613be
poorvasha <poorvashadmp@gmail.com>
2023-02-17 23:45:14 +0530

    implement secure local storage methods

---
+++ lib/models/local_storage_item.dart
@@ -0,0 +1,6 @@
+class LocalStorageItem {
+ LocalStorageItem(this.key, this.value);
+
+ final String key;
+ final String value;
+}
---
+++ lib/services/secure_local_storage.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_contacts/models/local_storage_item.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class SecureStorage {
+  final _secureStorage = const FlutterSecureStorage();
+
+  Future<void> writeSecureData(LocalStorageItem item) async {
+    try {
+      await _secureStorage.write(
+          key: item.key, value: item.value, aOptions: _getAndroidOptions());
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<String?> readSecureData(String key) async {
+    try {
+      String? data =
+          await _secureStorage.read(key: key, aOptions: _getAndroidOptions());
+      return data;
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
+    }
+  }
+
+  Future<void> deleteSecureData(String key) async {
+    try {
+      await _secureStorage.delete(key: key, aOptions: _getAndroidOptions());
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<List<LocalStorageItem>?> readAllSecureData() async {
+    try {
+      Map<String, String> allData =
+          await _secureStorage.readAll(aOptions: _getAndroidOptions());
+      List<LocalStorageItem> mappedData =
+          allData.entries.map((e) => LocalStorageItem(e.key, e.value)).toList();
+      return mappedData;
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return null;
+    }
+    
+  }
+
+  Future<void> deleteAllSecureData(String key) async {
+    try {
+      await _secureStorage.deleteAll(aOptions: _getAndroidOptions());
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<bool> containsKeyInSecureData(String key) async {
+    bool containsKey = await _secureStorage.containsKey(
+        key: key, aOptions: _getAndroidOptions());
+    return containsKey;
+  }
+
+  AndroidOptions _getAndroidOptions() =>
+      const AndroidOptions(encryptedSharedPreferences: true);
+}
