789f2b781430aec1fd9570338e527e5ee5dd8138
poorvasha <poorvashadmp@gmail.com>
2023-02-17 23:46:31 +0530

    implement IntialScreen UI and add route

---
+++ lib/screens/startup_screens/intial_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../utils/app_model.dart';
+import '../../utils/helpers.dart';
+import '../../utils/routes.dart' as routes;
+
+class InitalScreen extends StatefulWidget {
+  const InitalScreen({super.key});
+
+  @override
+  State<InitalScreen> createState() => _InitalScreenState();
+}
+
+class _InitalScreenState extends State<InitalScreen> {
+  @override
+  void initState() {
+    super.initState();
+    setIntialScreen();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+
+  void setIntialScreen() {
+    Helpers().checkIsUserLoggedIn().then((isUserLoggedIn) {
+      context.read<AppModel>().setInitialRoute =
+          isUserLoggedIn ? routes.Routes.contacts : routes.Routes.login;
+    });
+  }
+}
--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_contacts/screens/contacts.dart';
+import 'package:cloud_contacts/screens/startup_screens/intial_screen.dart';
 import 'package:cloud_contacts/screens/startup_screens/login_screen.dart';
 import 'package:cloud_contacts/screens/startup_screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
@@ -7,17 +8,20 @@ class Routes {
   static const String login = 'login';
   static const String signUp = 'signUp';
   static const String contacts = 'contacts';
+  static const String intial = 'intial';
 
   static Route<dynamic> controller(RouteSettings settings) {
-  switch (settings.name) {
-    case login:
-      return MaterialPageRoute(builder: (context) => const LoginScreen());
-    case signUp:
-      return MaterialPageRoute(builder: (context) => const SignUpScreen());
-    case contacts:
-      return MaterialPageRoute(builder: (context) => const ContactsScreen());
-    default:
-      throw('This route name does not exit');
+    switch (settings.name) {
+      case login:
+        return MaterialPageRoute(builder: (context) => const LoginScreen());
+      case signUp:
+        return MaterialPageRoute(builder: (context) => const SignUpScreen());
+      case contacts:
+        return MaterialPageRoute(builder: (context) => const ContactsScreen());
+      case intial:
+        return MaterialPageRoute(builder: (context) => const InitalScreen());
+      default:
+        return MaterialPageRoute(builder: (context) => const LoginScreen());
+    }
   }
-}
 }
