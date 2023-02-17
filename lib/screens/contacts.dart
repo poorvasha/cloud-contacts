836f98263a15fa78c76a0564959ce497e7d8c446
poorvasha <poorvashadmp@gmail.com>
2023-02-17 11:14:59 +0530

    Implement contacts screen UI

---
+++ lib/screens/contacts.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_contacts/utils/resources.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+import '../widgets/contact_block.dart';
+
+class ContactsScreen extends StatefulWidget {
+  const ContactsScreen({super.key});
+
+  @override
+  State<ContactsScreen> createState() => _ContactsScreenState();
+}
+
+class _ContactsScreenState extends State<ContactsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Theme.of(context).backgroundColor,
+        appBar: AppBar(
+            //centerTitle: true,
+            backgroundColor: Theme.of(context).primaryColor,
+            title: Text(
+              "Your Buddies",
+              style: AppTextStyles.semiBoldheaderStyle
+                  .copyWith(color: AppColors.lightGrey),
+              textAlign: TextAlign.center,
+            )),
+        body: Padding(
+          padding: AppResources.screenMargin.copyWith(top: 10),
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height - 135,
+                child: ListView.builder(
+                    itemCount: 15,
+                    itemBuilder: ((context, index) => const ContactBlock())),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: Container(
+          decoration: BoxDecoration(
+              color: Theme.of(context).backgroundColor,
+              borderRadius: BorderRadius.circular(30),
+              boxShadow: AppShaddows.addButtonShadow),
+          child: FloatingActionButton(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            onPressed: () {},
+            child: Icon(Icons.add_rounded,
+                shadows: AppShaddows.addIconShadow,
+                size: 40,
+                color: Theme.of(context).primaryColor),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
+  }
+}
---
+++ lib/widgets/contact_block.dart
@@ -0,0 +1,94 @@
+import 'package:cloud_contacts/screens/startup_screens/login_screen.dart';
+import 'package:cloud_contacts/utils/resources.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
+class ContactBlock extends StatefulWidget {
+  const ContactBlock({super.key});
+
+  @override
+  State<ContactBlock> createState() => _ContactBlockState();
+}
+
+class _ContactBlockState extends State<ContactBlock> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      decoration: BoxDecoration(
+        border: Border(
+            bottom: BorderSide(
+          color: AppColors.darkGrey.withOpacity(0.2),
+          width: 1,
+        )),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Poorvasha"),
+          FloatingActionButton(
+              mini: true,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              onPressed: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Theme.of(context).backgroundColor,
+                    borderRadius: BorderRadius.circular(35),
+                    boxShadow: AppShaddows.contactButtonShadow),
+                child: SpeedDial(
+                  iconTheme: IconThemeData(
+                      size: 24, shadows: AppShaddows.addIconShadow),
+                  spaceBetweenChildren: 20,
+                  direction: SpeedDialDirection.left,
+                  overlayOpacity: 0,
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  buttonSize: const Size(35, 35),
+                  gradientBoxShape: BoxShape.circle,
+                  childrenButtonSize: const Size(0, 0),
+                  overlayColor: Colors.transparent,
+                  foregroundColor: Theme.of(context).primaryColor,
+                  activeForegroundColor: Theme.of(context).backgroundColor,
+                  activeBackgroundColor: Theme.of(context).primaryColor,
+                  icon: Icons.arrow_back_ios_new_rounded,
+                  children: [
+                    SpeedDialChild(
+                      elevation: 0,
+                      onTap: () {},
+                      labelShadow: AppShaddows.contactButtonShadow,
+                      labelWidget: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).backgroundColor,
+                              borderRadius: BorderRadius.circular(50),
+                              boxShadow: AppShaddows.contactButtonShadow),
+                          child: Icon(Icons.phone_rounded, color: Theme.of(context).primaryColor)),
+                    ),
+                    SpeedDialChild(
+                        elevation: 0,
+                        foregroundColor: Colors.green,
+                        onTap: () {},
+                        labelShadow: AppShaddows.contactButtonShadow,
+                        labelWidget: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                                color: Theme.of(context).backgroundColor,
+                                borderRadius: BorderRadius.circular(50),
+                                boxShadow: AppShaddows.contactButtonShadow),
+                            child: Icon(
+                              Icons.whatsapp_rounded,
+                              color: Theme.of(context).primaryColor,
+                            )))
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
