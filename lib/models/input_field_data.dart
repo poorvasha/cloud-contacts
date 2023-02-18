a8ea26b45623c9f5ae9b9e5cbc4229c89a351424
poorvasha <poorvashadmp@gmail.com>
2023-02-18 20:58:36 +0530

    add new properties in input_field_data and resource files for field validation

--- lib/models/input_field_data.dart
+++ lib/models/input_field_data.dart
@@ -3,19 +3,28 @@ import 'package:flutter/services.dart';
 
 class InputFieldData {
   String? labelName;
-  String hintText;
   IconData? iconData;
+  String hintText;
   String errMessage;
   bool isValid;
   TextEditingController myController;
   TextInputType keyboardType;
   TextInputFormatter textInputType;
+  Function onTextChange;
+  bool obscureText;
+  bool showErrMessage;
 
-  InputFieldData(this.labelName, this.iconData,
-      {required this.hintText,
-      required this.errMessage,
-      required this.isValid,
-      required this.myController,
-      required this.keyboardType,
-      required this.textInputType});
+  InputFieldData({
+    this.labelName,
+    this.iconData,
+    required this.hintText,
+    required this.errMessage,
+    this.isValid = false,
+    required this.myController,
+    required this.keyboardType,
+    required this.textInputType,
+    required this.onTextChange,
+    this.obscureText = false,
+    this.showErrMessage = false,
+  });
 }
